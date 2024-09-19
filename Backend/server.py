4870fc8014551c9bea94ef914bd3bf73c4664832
Philip Hilding <philip.hilding@hotmail.com>
2024-09-20 00:11:06 +0200

    Implement and install flask_cors for cors middleware

--- Backend/server.py
+++ Backend/server.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from src.example.examples_routes import example_bp
 
 app = Flask(__name__)
+
+#Middleware
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
 
 app.register_blueprint(example_bp, url_prefix='/example')
 
