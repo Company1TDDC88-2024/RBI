ae995fd9ec71c8cd3ba72c0151fed5142bcb725c
Philip Hilding <philip.hilding@hotmail.com>
2024-09-19 23:26:03 +0200

    Remove old test service and routes that caused errors

--- Backend/src/test/test-routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from .test_service import TestService
-from . import test_bp
-
-@test_bp.route('/test-get', methods=['GET'])
-def test():
-    return TestService.test_get()
-
