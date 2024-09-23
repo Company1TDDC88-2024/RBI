a4c08b911f39ed7d23e8107a689c59a4a8cc579f
Marcus Lindholm <hello@marcuslindholm.com>
2024-09-23 12:45:51 +0200

    changed backend port to 5555

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "http://localhost:5555";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")!).render(
