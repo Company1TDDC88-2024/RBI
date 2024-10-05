4416fd1a0105f018fa2bbd3c6d64d988ee96d339
Marcus Lindholm <hello@marcuslindholm.com>
2024-10-05 18:37:55 +0200

    Changed to module.css for layout

--- Frontend/src/Components/Layout/Layout.tsx
+++ Frontend/src/Components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Layout as AntLayout } from "antd";
 import SiderMenu from "../SiderMenu/SiderMenu";
-import "./Layout.css";
+import styles from "./Layout.module.css";
 
 const { Header, Content, Footer } = AntLayout;
 
@@ -9,14 +9,14 @@ const Layout: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
   return (
     <AntLayout style={{ minHeight: "100vh" }}>
       <SiderMenu />
-      <AntLayout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }} />
+      <AntLayout className={styles.siteLayout}>
+        <Header className={styles.siteLayoutBackground} style={{ padding: 0 }} />
         <Content style={{ margin: "0 16px" }}>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+          <div className={styles.siteLayoutBackground} style={{ padding: 24, minHeight: 360 }}>
             {children}
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>Company 1 ©2024</Footer>
+        <Footer style={{ textAlign: "center" }}>Company1 © {new Date().getFullYear()}</Footer>
       </AntLayout>
     </AntLayout>
   );
