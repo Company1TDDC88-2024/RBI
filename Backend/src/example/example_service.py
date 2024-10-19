073b76a5cb3f9479495cf4776f936fb2b0b0e1de
Oscar Ljungdahl <osclj814@student.liu.se>
2024-10-19 22:47:49 +0200

    test

--- Backend/src/example/example_service.py
+++ Backend/src/example/example_service.py
@@ -1,5 +1,15 @@
+from src.database_connect import get_db_connection
+
 class ExampleService:
     def example_get(self):
         return {"firstWord": "Hello", "secondWord": "World"}
+    
+    async def connect(self):
+        conn = await get_db_connection()
+        print(conn)
+        if conn is None:
+            return "Failed to connect to database"
+        return "Connected to database"
+
     
 ExampleService = ExampleService()
