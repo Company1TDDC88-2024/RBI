055bd558d2c6745123308dd5c943d04a5ae64664
Oscar Ljungdahl <osclj814@student.liu.se>
2024-10-19 19:37:28 +0200

    f

--- Backend/server.py
+++ Backend/server.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=False)
 
 print("Server is running")
-app.register_blueprint(example_bp, url_prefix='api/example')
+app.register_blueprint(example_bp, url_prefix='/api/example')
 app.register_blueprint(customer_count_bp, url_prefix='/customer_count')
 app.register_blueprint(queue_count_bp, url_prefix='/queue_count')
 app.register_blueprint(coordinates_bp, url_prefix='/coordinates')
