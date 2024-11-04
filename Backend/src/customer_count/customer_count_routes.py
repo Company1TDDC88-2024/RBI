d10bc12789c7304d617df9fad290f37ba65fbd56
Truls Gripestam <trugr925@student.liu.se>
2024-11-04 15:19:01 +0100

    Customer count fix

--- Backend/src/customer_count/customer_count_routes.py
+++ Backend/src/customer_count/customer_count_routes.py
@@ -5,16 +5,43 @@ from datetime import datetime
 # Skapa en Blueprint
 customer_count_bp = Blueprint('customer_count', __name__)
 
-@customer_count_bp.route('/upload', methods=['POST'])
-async def upload_data():
-    data = request.json
-    
-    # Kontrollera att nödvändig data finns
-    if 'EnteringCustomers' not in data or 'ExitingCustomers' not in data or 'Timestamp' not in data:
-        return jsonify({'message': 'Missing EnteringCustomers or ExitingCustomers number of customers or Timestamp'}), 400
 
+@customer_count_bp.route('/upload', methods=['POST'])
+async def process_observations():
+    data = request.json
+
+    # Validate the required fields in the JSON
+    if 'id' not in data or 'observations' not in data:
+        return jsonify({'message': 'Missing required fields (id or observations)'}), 400
+    
+    # Validate the structure of each observation
+    try:
+        observations = data['observations']
+        bounding_boxes = [obs["bounding_box"] for obs in observations]
+        last_timestamp = observations[-1]["timestamp"]  # Get the last timestamp
+    except KeyError:
+        return jsonify({'message': 'Invalid data structure in observations'}), 400
+
+    # Determine whether the customer is entering or exiting based on bounding box positions
+    if  bounding_boxes[-1]["left"] < 0.3:
+        # Customer is exiting
+        result = {
+            "EnteringCustomers": 0,
+            "ExitingCustomers": 1,
+            "Timestamp": last_timestamp
+        }
+    elif bounding_boxes[-1]["right"] > 0.7:
+        # Customer is entering
+        result = {
+            "EnteringCustomers": 1,
+            "ExitingCustomers": 0,
+            "Timestamp": last_timestamp
+        }
+    print(result)
     result = await upload_data_to_db(data)
-    return jsonify({'message': result}) 
+    return jsonify(result), 200
+
+
 
 @customer_count_bp.route('/get', methods=['GET'])
 async def get_data():
