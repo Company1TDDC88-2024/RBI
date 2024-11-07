a14134ead27fd800d27f763a5e6bf003ae24f661
Oscar Ljungdahl <osclj814@student.liu.se>
2024-11-07 17:45:50 +0100

    into development test

--- Backend/server.py
+++ Backend/server.py
@@ -8,28 +8,28 @@ from src.example.date_routes import date_bp
 from src.login.login_routes import login_bp
 import secrets
 import logging
+import os
+from dotenv import load_dotenv
 
 
 app = Flask(__name__)
+if(os.getenv('Deployment') == 'True'):
+    port = 80
+else:
+    port = 5555
 
-<<<<<<< HEAD
-# Middleware
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=False)
-
-=======
 app.secret_key = secrets.token_hex(16)
 
-
 # Middleware
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=False)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
->>>>>>> 2a09a50f49875fa31189ee2fc2a6ca15a5fb22eb
 print("Server is running")
 app.register_blueprint(example_bp, url_prefix='/api/example')
 app.register_blueprint(customer_count_bp, url_prefix='/api/customer_count')
 app.register_blueprint(queue_count_bp, url_prefix='/api/queue_count')
 app.register_blueprint(coordinates_bp, url_prefix='/api/coordinates')
 app.register_blueprint(date_bp, url_prefix='/api/date')
+app.register_blueprint(login_bp, url_prefix='/api/login')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import axios from "axios";
 
-axios.defaults.baseURL =
-    "https://tddc88-company1-ht24.kubernetes-public.it.liu.se/api";
+const baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = baseURL;
+
+    //"https://tddc88-company1-ht24.kubernetes-public.it.liu.se/api";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")!).render(
