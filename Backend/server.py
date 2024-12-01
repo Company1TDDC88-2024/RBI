5b05acb3dd7c3dd64f13a7e88289c6b7d4c14c61
Marcus Lindholm <hello@marcuslindholm.com>
2024-12-02 00:34:31 +0100

    Uncommented Redis limiter for prod

--- Backend/server.py
+++ Backend/server.py
@@ -28,13 +28,13 @@ app.secret_key = secrets.token_hex(16)
 # Redis client, used for storing session data for the limiter
 app.config['SESSION_REDIS'] = Redis(host='redis', port=6379)
 
-# # Limiter for requests to prevent DDOS attacks
-# limiter = Limiter(
-#     key_func=get_remote_address,
-#     app=app,
-#     storage_uri=redis_uri,
-#     default_limits=["1000 per minute"] # Limits: ["x requests per hour, y requests per minute"]
-# )
+# Limiter for requests to prevent DDOS attacks
+limiter = Limiter(
+    key_func=get_remote_address,
+    app=app,
+    storage_uri=redis_uri,
+    default_limits=["1000 per minute"] # Limits: ["x requests per hour, y requests per minute"]
+)
 
 # Middleware
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
